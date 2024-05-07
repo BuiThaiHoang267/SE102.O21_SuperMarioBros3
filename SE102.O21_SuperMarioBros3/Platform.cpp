dae511154609908f2320216e57d6a886f94cfe1f
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-05-07 15:09:44 +0700

    Add and draw land, create scene level1

--- SE102.O21_SuperMarioBros3/Platform.cpp
+++ SE102.O21_SuperMarioBros3/Platform.cpp
@@ -44,7 +44,7 @@ void CPlatform::Render()
 	if (length>1)
 		s->Get(this->spriteIdEnd)->Draw(xx, y);
 
-	RenderBoundingBox();
+	//RenderBoundingBox();
 }
 
 void CPlatform::GetBoundingBox(float& l, float& t, float& r, float& b)
@@ -58,6 +58,7 @@ void CPlatform::GetBoundingBox(float& l, float& t, float& r, float& b)
 
 int CPlatform::IsDirectionColliable(float nx, float ny)
 {
-	if (nx == 0 && ny == -1) return 1;
-	else return 0;
+	/*if (nx == 0 && ny == -1) return 1;
+	else return 0;*/
+	return 1;
 }
