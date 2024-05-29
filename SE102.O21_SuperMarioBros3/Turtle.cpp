2a3a4f522e0f41332c921db2ee92980dec0e1515
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-05-29 15:18:41 +0700

    define function for turtle and create CheckMove obj

--- SE102.O21_SuperMarioBros3/Turtle.cpp
+++ SE102.O21_SuperMarioBros3/Turtle.cpp
@@ -2,19 +2,114 @@
 
 void CTurtle::Render()
 {
+	aniId = GetAniId();
 	CAnimations* animations = CAnimations::GetInstance();
-	animations->Get(400000)->Render(x,y);
+	animations->Get(aniId)->Render(x,y);
+	//RenderBoundingBox();
 }
 
 void CTurtle::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
 	l = x - TURTLE_BBOX_WIDTH / 2 + 1;
-	t = y - TURTLE_BBOX_HEIGHT / 2;
+	t = y - TURTLE_BBOX_HEIGHT / 2 + offsetYBBox;
 	r = l + TURTLE_BBOX_WIDTH - 1;
 	b = t + TURTLE_BBOX_HEIGHT;
 }
 
+void CTurtle::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	vy += ay * dt;
+	if (state == TURTLE_STATE_TORTOISESHELL && GetTickCount64() - tortoiseshell_start > TURTLE_RETURN_TIMEOUT)
+	{
+		SetState(TURTLE_STATE_WAKEUP);
+	}
+
+	if (state == TURTLE_STATE_WAKEUP && GetTickCount64() - wakeup_start > TURTLE_WAKEUP_TIMEOUT)
+	{
+		SetState(TURTLE_STATE_WALK);
+	}
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
 int CTurtle::GetAniId()
 {
-	return 1;
+	if (state == TURTLE_STATE_WALK)
+	{
+		if (vx <= 0)
+			return ID_ANI_TURTLE_WALK_LEFT;
+		else
+			return ID_ANI_TURTLE_WALK_RIGHT;
+	}
+	else if (state == TURTLE_STATE_TORTOISESHELL)
+	{
+		return ID_ANI_TURTLE_TORTOISESHELL;
+	}
+	else if (state == TURTLE_STATE_RUN)
+	{
+		if (vx <= 0)
+			return ID_ANI_TURTLE_RUN_LEFT;
+		else
+			return ID_ANI_TURTLE_RUN_RIGHT;
+	}
+	else if (state == TURTLE_STATE_WAKEUP)
+	{
+		return ID_ANI_TURTLE_WAKEUP;
+	}
+	else {
+		return ID_ANI_TURTLE_TORTOISESHELL;
+	}
+}
+
+void CTurtle::OnCollisionWith(LPCOLLISIONEVENT e)
+{
+	if (!e->obj->IsBlocking()) return;
+	if (dynamic_cast<CTurtle*>(e->obj)) return;
+
+	if (e->ny != 0)
+	{
+		vy = 0;
+	}
+	else if (e->nx != 0)
+	{
+		vx = -vx;
+	}
+}
+
+void CTurtle::OnNoCollision(DWORD dt)
+{
+	x += vx * dt;
+	y += vy * dt;
+}
+
+void CTurtle::SetState(int state)
+{
+	this->state = state;
+	if (state == TURTLE_STATE_WALK)
+	{
+		offsetYBBox = 4;
+		y -= 4;
+		vx = TURTLE_VX_STATE_WALK;
+		ay = TURTLE_GRAVITY;
+	}
+	else if (state == TURTLE_STATE_TORTOISESHELL)
+	{
+		y += 4;
+		tortoiseshell_start = GetTickCount64();
+		offsetYBBox = 0;
+		vx = 0;
+	}
+	else if (state == TURTLE_STATE_RUN)
+	{
+		offsetYBBox = 0;
+		vx = TURTLE_VX_STATE_RUN;
+	}
+	else if (state == TURTLE_STATE_WAKEUP)
+	{
+		wakeup_start = GetTickCount64();
+		offsetYBBox = 0;
+		vx = 0;
+		vy = 0;
+		ay = 0;
+	}
 }
--- SE102.O21_SuperMarioBros3/Turtle.h
+++ SE102.O21_SuperMarioBros3/Turtle.h
@@ -4,26 +4,60 @@
 #include "Animation.h"
 #include "Animations.h"
 
-#define ID_ANI_TURTLE_IDLE 430000
+#define ID_ANI_TURTLE_TORTOISESHELL 430000
 
 #define ID_ANI_TURTLE_WALK_LEFT 400000
-#define ID_ANI_TURTLE_WALK_RIGHT 410000
+#define ID_ANI_TURTLE_WALK_RIGHT 400001
 
-#define ID_ANI_TURTLE_RUN_LEFT 400001
+#define ID_ANI_TURTLE_RUN_LEFT 410000
 #define ID_ANI_TURTLE_RUN_RIGHT 410001
 
-#define ID_ANI_TURTLE_WAKEUP_LEFT 420000
+#define ID_ANI_TURTLE_WAKEUP 420000
 
 #define TURTLE_BBOX_WIDTH 16
 #define TURTLE_BBOX_HEIGHT 16
 
+#define TURTLE_STATE_WALK 1
+#define TURTLE_STATE_TORTOISESHELL 2
+#define TURTLE_STATE_RUN 3
+#define TURTLE_STATE_WAKEUP 4
+
+#define TURTLE_VX_STATE_WALK 0.05f
+#define TURTLE_VX_STATE_RUN 0.15f
+
+#define TURTLE_GRAVITY 0.02f
+
+#define TURTLE_RETURN_TIMEOUT 5000
+#define TURTLE_WAKEUP_TIMEOUT 2500
+
+
 class CTurtle : public CGameObject {
+private:
+	float ay;
+	int aniId;
+	int offsetYBBox;
+	ULONGLONG tortoiseshell_start;
+	ULONGLONG wakeup_start;
 public:
-	CTurtle(float x, float y) : CGameObject(x, y) {}
+	CTurtle(float x, float y) : CGameObject(x, y) 
+	{
+		this->tortoiseshell_start = -1;
+		this->ay = TURTLE_GRAVITY;
+		this->aniId = ID_ANI_TURTLE_WALK_LEFT;
+		this->offsetYBBox = 0;
+		this->wakeup_start = 0;
+		//SetState(TURTLE_STATE_WALK);
+		//SetState(TURTLE_STATE_RUN);
+		SetState(TURTLE_STATE_TORTOISESHELL);
+		//SetState(TURTLE_STATE_RUN);
+	}
 	void Render();
-	void Update(DWORD dt) {}
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	int IsBlocking() { return 1; }
 	int IsCollidable() { return 1; };
+	void OnNoCollision(DWORD dt);
+	void OnCollisionWith(LPCOLLISIONEVENT e);
+	void SetState(int state);
 	int GetAniId();
 };
