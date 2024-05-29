06e5f655071f1e5b492798dc6dc4e6ad52fa16e0
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-05-30 06:59:04 +0700

    Spawn success

--- SE102.O21_SuperMarioBros3/EnemyManager.cpp
+++ SE102.O21_SuperMarioBros3/EnemyManager.cpp
@@ -1,10 +1,34 @@
 #include "EnemyManager.h"
+#include "PlayScene.h"
+#include "Goomba.h"
 
 void CEnemyManager::Render()
 {
-
+	spawn3Goomba->Render();
 }
 void CEnemyManager::GetBoundingBox(float& l, float& t, float& r, float& b) 
 {
 
+}
+
+void CEnemyManager::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	spawn3Goomba->Update(dt, coObjects);
+	SpawnEnemy();
+}
+
+void CEnemyManager::SpawnEnemy()
+{
+	if (spawn3Goomba->GetIsDetectedMario() && isSpawn3Goomba == false)
+	{
+		isSpawn3Goomba = true;
+		LPSCENE s = CGame::GetInstance()->GetCurrentScene();
+		LPPLAYSCENE p = dynamic_cast<CPlayScene*>(s);
+		LPGAMEOBJECT goomba1 = new CGoomba(850, 128);
+		p->AddGameObject(goomba1);
+		LPGAMEOBJECT goomba2 = new CGoomba(950, 128);
+		p->AddGameObject(goomba2);
+		LPGAMEOBJECT goomba3 = new CGoomba(1050, 128);
+		p->AddGameObject(goomba3);
+	}
 }
--- SE102.O21_SuperMarioBros3/EnemyManager.h
+++ SE102.O21_SuperMarioBros3/EnemyManager.h
@@ -3,13 +3,22 @@
 #include "GameObject.h"
 #include "Animation.h"
 #include "Animations.h"
+#include "OnTriggerSpawnEnemy.h"
 
 
 class CEnemyManager : public CGameObject {
+protected:
+	COnTriggerSpawnEnemy* spawn3Goomba;
+	bool isSpawn3Goomba;
 public:
-	CEnemyManager(float x, float y) : CGameObject(x, y) {}
+	CEnemyManager(float x, float y) : CGameObject(x, y) 
+	{
+		spawn3Goomba = new COnTriggerSpawnEnemy(711, 80, 20, 120);
+		isSpawn3Goomba = false;
+	}
 	void Render();
-	void Update(DWORD dt) {}
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
 	int IsBlocking() { return 0; }
+	void SpawnEnemy();
 };
